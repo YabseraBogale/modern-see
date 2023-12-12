b503403397c7c70e4a1948dab99e837107cf25e4
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-12 16:01:52 +0300

    ok

--- pointer.cpp
+++ pointer.cpp
@@ -5,8 +5,8 @@ int main(){
     const int* y=&x;
     std::cout<<"Before X "<<x<<" Y "<<*y<<std::endl;
     // this works b/c we are changing the x whic is mutable but
-    // y=9; does work since it  immutable 
-    x=9;
+    // y=9; does work since it  immutable it is readonly
+    *y=9;
     std::cout<<"After X "<<x<<" Y "<<*y<<std::endl;
     return 0;
 }
