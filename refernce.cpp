2753ad025091129c1e12a45309192df96ca78204
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-12 16:00:49 +0300

    ok

--- refernce.cpp
+++ refernce.cpp
@@ -4,6 +4,8 @@ int main(){
     int x=10;
     const int& y=x;
     std::cout<<"Before X "<<x<<" Y "<<y<<std::endl;
+    // this works b/c we are changing the x whic is mutable but
+    // y=9; does work since it  immutable 
     x=9;
     std::cout<<"After X "<<x<<" Y "<<y<<std::endl;
     return 0;
