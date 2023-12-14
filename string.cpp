8cb048fb6e052d12915a46389844ba383e7f0a77
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-14 16:51:11 +0300

    ok

--- string.cpp
+++ string.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
-
+#include<typeinfo>
 int main(){
     std::string s="";
     std::getline(std::cin,s);
     std::cout<<s<<std::endl;
+    std::cout<<typeid(s.find("h")).name()<<std::endl;
     return 0;
 }
