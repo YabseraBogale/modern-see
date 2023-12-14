d34ce0bc8c645af4acfb445edb6aae01b3fd8625
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-14 20:38:36 +0300

    ok

--- bitset.cpp
+++ bitset.cpp
@@ -1,11 +1,7 @@
 #include<iostream>
 #include<bitset>
 int main(){
-    int num=0;
-    std::cout<<"Enter number: ";
-    std::cin>>num;
-    std::bitset<4> fromuser(num);
-    std::cout<<fromuser<<std::endl;
+    std::bitset<4> fromuser(5);
     std::bitset<4> machine(4);
     std::bitset<4> added;
     added=machine | fromuser;
