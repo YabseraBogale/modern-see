7ce2b0941c889214bda0e02307ad3b32eec7bc39
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-19 07:53:55 +0300

    ok

--- memeberlist.cpp
+++ memeberlist.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <ostream>
 class Shape{
     float width;
     float hieght;
@@ -6,10 +7,13 @@ class Shape{
         Shape(float x,float y) :width{x},hieght{y}{
 
         }
-
+        float GetWidth(){ return width;}
+        float GetHieght(){  return hieght;}
 };
 int main(){
     Shape rec{12,13};
-
+    Shape tri=rec;
+    std::cout<<"rec x: "<<rec.GetWidth()<<" y: "<<rec.GetHieght()<<std::endl;
+    std::cout<<"tri x: "<<tri.GetWidth()<<" y: "<<tri.GetHieght()<<std::endl;
     return 0;
 }
