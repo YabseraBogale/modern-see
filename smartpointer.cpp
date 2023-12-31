199456be66f14f7434300cfde3b5be19cf29891c
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-12-31 15:54:19 +0300

    ok

--- smartpointer.cpp
+++ smartpointer.cpp
@@ -5,6 +5,8 @@ int main(){
 
     std::unique_ptr<int> p= std::make_unique<int>(123);
     std::cout<<*p<<std::endl;
-
+    int k=static_cast<int>(50.25);
+    bool b=int(50.25)==k;
+    std::cout<<b<<std::endl;
     return 0;
 }
