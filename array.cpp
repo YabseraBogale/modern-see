b01e2b3b88f98fb6162ba3a96f4042bb7c0c8fbd
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-01-01 21:56:55 +0300

    ok

--- array.cpp
+++ array.cpp
@@ -1,11 +1,7 @@
 #include<iostream>
 #include <vector>
-import mymodule;
 int main(){
    std::vector<int>arr={1,2,3,4,5};
    std::cout<<sizeof(arr)/sizeof(arr[0])<<"\n";
-   int x = myfunction();
-   std::cout<<x<<std::endl;
-
    return 0;
 }
