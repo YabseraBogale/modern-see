fce1705fb4a7b11cc0b488b98202fcc151dc2d8f
YabseraBogale <yabserapython@gmail.com>
2024-11-27 20:20:56 +0300

    ok

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -13,8 +13,7 @@ int main() {
             auto parse=req.get_body_params();
             // Extract form fields
             std::string username = parse.get("username") ? parse.get("username") : "No username";
-            std::string password = parse.get("password") ? parse.get("password") : "No password";
-            
+            std::string password = parse.get("password") ? parse.get("password") : "No password";            
             return "username: " + username + " password: " + password;
         } 
 
@@ -22,5 +21,17 @@ int main() {
         auto index = crow::mustache::load_text("index.html");
         return index;
     });
+
+    CROW_ROUTE(app,"/chat").methods(crow::HTTPMethod::GET,crow::HTTPMethod::POST)([](const crow::request& request,const crow::response& response){
+        if(request.method==crow::HTTPMethod::POST){
+
+        } else if(request.method==crow::HTTPMethod::GET){
+
+        } else{
+            return std::to_string(501);
+        }
+    });
+
+
     app.port(8080).run();
 }
