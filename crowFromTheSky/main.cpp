d4df356852a22a36240068d5fecae7e512ff4484
YabseraBogale <yabserapython@gmail.com>
2024-11-27 11:45:14 +0300

    ok

--- crowFromTheSky/main.cpp
+++ crowFromTheSky/main.cpp
@@ -3,7 +3,7 @@
 int main(){
     crow::SimpleApp app;
     CROW_ROUTE(app,"/")([](){
-        auto index=crow::mustache::load_text("index,html");
+        auto index=crow::mustache::load_text("index.html");
         return index;
     });
     app.port(8080).run();
